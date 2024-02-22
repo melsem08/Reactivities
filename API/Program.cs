291d1e72f519822a8c517444136c56663bc032aa
Semen Melnykov <gersen18@gmail.com>
2024-02-22 23:45:24 +0000

    Error Message Changed

--- API/Program.cs
+++ API/Program.cs
@@ -39,7 +39,7 @@ try
 catch (Exception ex)
 {
     var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex, "An error occured during migration");
+    logger.LogError(ex, "An error occured during migration...");
 }
 
 app.Run();
