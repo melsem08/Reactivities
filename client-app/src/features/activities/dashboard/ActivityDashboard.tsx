466bdd11764a1b264d6ef6839b9732094b45e0c1
Semen Melnykov <gersen18@gmail.com>
2024-03-03 23:59:30 +0000

    CRUD Operations Using API Added

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,10 +15,11 @@ interface Props{
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void; 
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({activities, selectedActivity, selectActivity, cancelSelectedActivity,
-    editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props) {
+    editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting}: Props) {
     return (
         <Grid>
             <GridColumn width='10'>
@@ -32,7 +33,12 @@ export default function ActivityDashboard({activities, selectedActivity, selectA
                 openForm={openForm}
                 />}
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+                <ActivityForm 
+                closeForm={closeForm} 
+                activity={selectedActivity} 
+                createOrEdit={createOrEdit} 
+                submitting={submitting}
+                />}
             </GridColumn>
         </Grid>
     )
