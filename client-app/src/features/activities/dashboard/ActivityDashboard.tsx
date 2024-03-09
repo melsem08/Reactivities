64b66002e11899d9092addb3b505f6cfedf19880
Semen Melnykov <gersen18@gmail.com>
2024-03-09 23:54:18 +0000

    Loading Message Changed

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,7 +14,7 @@ export default observer (function ActivityDashboard() {
       if (actvityRegistry.size <= 1) activityStore.loadActivities();
     }, [loadActivities, actvityRegistry.size])
   
-    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app...' />
 
     return (
         <Grid>
