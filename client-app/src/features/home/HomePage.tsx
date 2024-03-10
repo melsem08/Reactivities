ae5933f387b08016ef045c7991acdebf156562a9
Semen Melnykov <gersen18@gmail.com>
2024-03-10 23:25:24 +0000

    Header Change

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -5,7 +5,7 @@ export default function HomePage(){
     return(
         <Container style={{marginTop: '7em'}}>
             <h1>Home Page</h1>
-            <h3>Go to <Link to='/activities'>Activities</Link></h3>
+            <h3>Go To <Link to='/activities'>Activities</Link></h3>
         </Container>
     )
 }
