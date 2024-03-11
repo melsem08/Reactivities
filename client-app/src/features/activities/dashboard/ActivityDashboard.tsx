88e4bbfe37f947b334bf69e625306560147fb405
Semen Melnykov <gersen18@gmail.com>
2024-03-11 23:56:40 +0000

    Styling Added

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,6 +4,7 @@ import { Grid, GridColumn } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
 import { useStore } from "../../../app/stores/store";
 import LoadingComponent from "../../../app/layout/LoadingComponents";
+import ActivityFilters from "./ActivityFilters";
 
 export default observer (function ActivityDashboard() {
 
@@ -22,7 +23,7 @@ export default observer (function ActivityDashboard() {
                 <ActivityList />
             </GridColumn>
             <GridColumn width='6'>
-                <h2>Activity filters</h2>
+                <ActivityFilters />
             </GridColumn>
         </Grid>
     )
