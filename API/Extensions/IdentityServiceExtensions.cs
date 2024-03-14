9220784ac9c716b11e848275cfd2c16b09446f09
Semen Melnykov <gersen18@gmail.com>
2024-03-14 03:40:54 +0000

    Identity Logic Added

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,3 +1,4 @@
+using API.Services;
 using Domain;
 using Persistence;
 
@@ -12,6 +13,7 @@ namespace API.Extensions
             }).AddEntityFrameworkStores<DataContext>();
 
             services.AddAuthentication();
+            services.AddScoped<TokenService>();
 
             return services;
         }
