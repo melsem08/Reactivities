ec8d34b065eb4380a5e40a1559acbaa58bfcb5d8
Semen Melnykov <gersen18@gmail.com>
2024-03-30 23:56:00 +0000

    Endpoint Comment Added

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -42,7 +42,7 @@ namespace API.Controllers
             return HandleResult(await Mediator.Send(new Delete.Command {Id = id}));
         }
 
-        [HttpPost("{id}/attend")]
+        [HttpPost("{id}/attend")] //api/{id}/attend
         public async Task<IActionResult> Attend(Guid id)
         {
             return HandleResult(await Mediator.Send(new UpdateAttendance.Command {Id = id}));
